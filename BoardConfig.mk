62cf85c179cc2ca3e91d991d21d735b9e486c035
Mathew Daniel <mathew.daniel@nexwave.ca>
2013-03-07 14:30:36 -0500

    Modifications for twrp
    Change-Id: I27f1cfdd1b67a378551abebc2252989302f34fc4

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -12,3 +12,13 @@ TARGET_KERNEL_CONFIG := cyanogenmod_su640_defconfig
 TARGET_PREBUILT_KERNEL := device/lge/su640/kernel
 
 BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := device/lge/su640/bluetooth
+
+# TWRP flags
+DEVICE_RESOLUTION := 720x1280
+RECOVERY_GRAPHICS_USE_LINELENGTH := true
+TARGET_RECOVERY_PIXEL_FORMAT := "RGBX_8888"
+BOARD_HAS_NO_REAL_SDCARD := true
+TW_NO_USB_STORAGE := true
+TW_NO_REBOOT_BOOTLOADER := true
+#TARGET_USE_CUSTOM_LUN_FILE_PATH := /sys/devices/virtual/android_usb/android0/f_mass_storage/lun/file
+TARGET_USE_CUSTOM_LUN_FILE_PATH := /sys/devices/platform/msm_hsusb/gadget/lun0/file
